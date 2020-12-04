102d6fe065c0d6a1d4808be49844c440c2913110
ckorzen <ckorzen@users.noreply.github.com>
2020-12-04 10:50:03 +0100

    Added two more tests regarding the extraction of figures and shapes.

--- src/test/java/textextraction/pdfparser/TestPdfParserAcl2004.java
+++ src/test/java/textextraction/pdfparser/TestPdfParserAcl2004.java
@@ -2,14 +2,18 @@ package textextraction.pdfparser;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import textextraction.common.models.Character;
 import textextraction.common.models.Color;
 import textextraction.common.models.Document;
+import textextraction.common.models.Figure;
 import textextraction.common.models.Font;
 import textextraction.common.models.Position;
+import textextraction.common.models.Shape;
 import textextraction.pdfparser.exception.PdfParserException;
 
 public class TestPdfParserAcl2004 {
@@ -63,5 +67,47 @@ public class TestPdfParserAcl2004 {
     assertEquals("10.0", String.format("%.1f", fontsize));
     color = character.getColor();
     assertEquals("Color([0, 0, 0])", color.toString());
+  }
+
+  /**
+   * Tests the extraction results on the level of figures.
+   */
+  @Test
+  public void testFiguresExtraction() {
+    List<Figure> figures = pdf.getPages().get(0).getFigures();
+    assertEquals(0, figures.size());
+
+    figures = pdf.getPages().get(1).getFigures();
+    assertEquals(0, figures.size());
+
+    figures = pdf.getPages().get(2).getFigures();
+    assertEquals(0, figures.size());
+  }
+
+  /**
+   * Tests the extraction results on the level of shapes.
+   */
+  @Test
+  public void testShapesExtraction() {
+    List<Shape> shapes = pdf.getPages().get(0).getShapes();
+    assertEquals(0, shapes.size());
+
+    shapes = pdf.getPages().get(1).getShapes();
+    assertEquals(805, shapes.size());
+
+    Shape shape = shapes.get(0);
+    Position pos = shape.getPosition();
+    assertEquals("Pos(p: 2, bb: Rect(128.748,482.74802,140.868,482.268))", pos.toString());
+
+    shape = shapes.get(7);
+    pos = shape.getPosition();
+    assertEquals("Pos(p: 2, bb: Rect(496.815,611.55,496.815,717.175))", pos.toString());
+
+    shapes = pdf.getPages().get(2).getShapes();
+    assertEquals(437, shapes.size());
+
+    shape = shapes.get(10);
+    pos = shape.getPosition();
+    assertEquals("Pos(p: 3, bb: Rect(174.065,611.525,174.065,612.875))", pos.toString());
   }
 }
--- src/test/java/textextraction/pdfparser/TestPdfParserKi2018.java
+++ src/test/java/textextraction/pdfparser/TestPdfParserKi2018.java
@@ -2,14 +2,18 @@ package textextraction.pdfparser;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import textextraction.common.models.Character;
 import textextraction.common.models.Color;
 import textextraction.common.models.Document;
+import textextraction.common.models.Figure;
 import textextraction.common.models.Font;
 import textextraction.common.models.Position;
+import textextraction.common.models.Shape;
 import textextraction.pdfparser.exception.PdfParserException;
 
 public class TestPdfParserKi2018 {
@@ -62,6 +66,66 @@ public class TestPdfParserKi2018 {
     fontsize = character.getFontFace().getFontSize();
     assertEquals("8.5", String.format("%.1f", fontsize));
     color = character.getColor();
+    assertEquals("Color([0, 0, 0])", color.toString());
+  }
+
+  /**
+   * Tests the extraction results on the level of figures.
+   */
+  @Test
+  public void testFiguresExtraction() {
+    List<Figure> figures = pdf.getPages().get(0).getFigures();
+    assertEquals(23, figures.size());
+
+    Figure figure = figures.get(0);
+    Position pos = figure.getPosition();
+    assertEquals("Pos(p: 1, bb: Rect(471.27173,738.1811,502.88696,769.6623))", pos.toString());
+
+    figure = figures.get(15);
+    pos = figure.getPosition();
+    assertEquals("Pos(p: 1, bb: Rect(482.32263,762.32495,483.30765,763.366))", pos.toString());
+
+    figures = pdf.getPages().get(1).getFigures();
+    assertEquals(1, figures.size());
+
+    figure = figures.get(0);
+    pos = figure.getPosition();
+    assertEquals("Pos(p: 2, bb: Rect(53.785667,472.5781,541.4897,960.31854))", pos.toString());
+
+    figures = pdf.getPages().get(2).getFigures();
+    assertEquals(0, figures.size());
+  }
+
+  /**
+   * Tests the extraction results on the level of shapes.
+   */
+  @Test
+  public void testShapesExtraction() {
+    List<Shape> shapes = pdf.getPages().get(0).getShapes();
+    assertEquals(253, shapes.size());
+
+    shapes = pdf.getPages().get(1).getShapes();
+    assertEquals(5, shapes.size());
+
+    Shape shape = shapes.get(0);
+    Position pos = shape.getPosition();
+    assertEquals("Pos(p: 2, bb: Rect(51.0236,745.7852,544.2516,745.7852))", pos.toString());
+    Color color = shape.getColor();
+    assertEquals("Color([0, 0, 0])", color.toString());
+
+    shape = shapes.get(3);
+    pos = shape.getPosition();
+    assertEquals("Pos(p: 2, bb: Rect(544.252,733.11,51.024,733.11))", pos.toString());
+    color = shape.getColor();
+    assertEquals("Color([255, 255, 255])", color.toString());
+
+    shapes = pdf.getPages().get(2).getShapes();
+    assertEquals(5, shapes.size());
+
+    shape = shapes.get(4);
+    pos = shape.getPosition();
+    assertEquals("Pos(p: 3, bb: Rect(306.1417,85.4299,351.2517,85.4299))", pos.toString());
+    color = shape.getColor();
     assertEquals("Color([0, 0, 0])", color.toString());
   }
 }
